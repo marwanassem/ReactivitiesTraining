3ff2a432332972d6c61958c2ab849a42fbfd262a
marwanassem <marwan.assem1@gmail.com>
2025-01-08 22:16:27 +0200

    End of section 12

--- Persistence/Seed.cs
+++ Persistence/Seed.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Domain;
+using Microsoft.AspNetCore.Identity;
 
 namespace Persistence
 {
     public class Seed
     {
-        public static async Task SeedData(DataContext context)
+        public static async Task SeedData(DataContext context, UserManager<AppUser> userManager)
         {
+            if(!userManager.Users.Any()) {
+                var users = new List<AppUser> {
+                    new AppUser{DisplayName="Marwan", UserName="marwan", Email="marwan@test.com"},
+                    new AppUser{DisplayName="Reem", UserName="reem", Email="reem@test.com"},
+                    new AppUser{DisplayName="Ahmed", UserName="ahmed", Email="ahmed@test.com"},
+                };
+
+                foreach (var user in users) {
+                    await userManager.CreateAsync(user, "Pa$$w0rd");
+                }
+            }
             if (context.Activities.Any()) return;
             
             var activities = new List<Activity>
